7cd3ca55efeb41d5acebc1f689a35a8094d7e089
kulichen <kulichen@cs.petrsu.ru>
2021-12-30 16:36:54 +0300

    5

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-/**
+  /**
  * floor.c -- программа для расчета нужного этажа
  *
  * Copyright (c) 2009, Student Name <student@cs.karelia.ru>
@@ -23,11 +23,11 @@ int main()
 
     /* Запрашиваем число квартир на этаже */
     printf             ("Введите          число квартир на каждом этаже: ");
-    scanf("%d", &flats_per_floor);
+                  scanf("%d", &flats_per_floor);
 
-    /* Рассчитываем и выводим номер этажа */
+                            /* Рассчитываем и выводим номер этажа */
                 printf("Вам нужно подняться на %d этаж\n", (flat_number+flats_per_floor-1) / flats_per_floor);
     
-    return 0
+     return 0
 	;
 }
