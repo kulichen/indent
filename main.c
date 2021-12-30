969d506e13f961a0a983a238e5080cc88239c09e
kulichen <kulichen@cs.petrsu.ru>
2021-12-30 16:32:33 +0300

    Third

--- main.c
+++ main.c
@@ -19,14 +19,15 @@ int main()
  
     /* Запрашиваем квартиру, в которой проживает адресат */
     printf         ("Введите номер интересующей квартиры: ");
-    scanf("%d", &flat_number);
+              scanf("%d", &flat_number);
 
     /* Запрашиваем число квартир на этаже */
-    printf("Введите          число квартир на каждом этаже: ");
+    printf             ("Введите          число квартир на каждом этаже: ");
     scanf("%d", &flats_per_floor);
 
     /* Рассчитываем и выводим номер этажа */
-    printf("Вам нужно подняться на %d этаж\n", (flat_number+flats_per_floor-1) / flats_per_floor);
+                printf("Вам нужно подняться на %d этаж\n", (flat_number+flats_per_floor-1) / flats_per_floor);
     
-    return 0;
+    return 0
+	;
 }
